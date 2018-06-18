f2f340cf952c3a0036b680c961f083e20c76a43f
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-06-18 16:19:07 -0600

    updated queries

--- queries.js
+++ queries.js
@@ -1,14 +1,26 @@
 const database = require("./database-connection");
 
 module.exports = {
-    list(){
-    },
-    read(id){
-    },
-    create(coffee){
-    },
-    update(id, coffee){
-    },
-    delete(id){
-    }
+  list() {
+    return database('coffee').select();
+  },
+  read(id) {
+    return database("coffee").select().where("id", id).first();
+  },
+  create(coffee) {
+    return database("resolutions")
+      .insert(resolution)
+      .returning("*")
+      .then(record => record[0]);
+  },
+  update(id, coffee) {
+    return database("coffee")
+      .update(resolution)
+      .where("id", id)
+      .returning("*")
+      .then(record => record[0]);
+  },
+  delete(id) {
+    return database("coffee").delete().where("id", id);
+  }
 };
