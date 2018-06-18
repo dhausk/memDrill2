bfb2c365cf518f3e784362cd8bea9a84f7141144
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-06-18 16:13:18 -0600

    updated json

--- app.js
+++ app.js
@@ -10,14 +10,12 @@ app.use(bodyParser.json());
 
 app.use("/coffees", coffees);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
     const err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
-// error handler
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({
